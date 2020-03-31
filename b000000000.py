ba7dc8f93efcb7503325eec2561160ef06816664
willfrid boris <boriskwayep35@yahoo.com>
2020-03-31 14:31:32 -0400

    La taille de la queue est <4>

--- b000000000.py
+++ b000000000.py
@@ -3,9 +3,30 @@
 
 @author: BORIS KWAYEP
 """
+eleves = {}
+eleves["Boris"]=["Amir","Franck","Nathalie","Bertrand"]
+eleves["Amir"]=[]
+eleves["Franck"]=[]
+eleves["Nathalie"]=[]
+eleves["Bertrand"]=["Erna","Hassana","Abdelkrim"]
+eleves["Erna"]=[]
+eleves["Hassana"]=[]
+eleves["Zoureni"]=["Sekou","Auriane","Corlings"]
+eleves["Sekou"]=[]
+eleves["Auriane"]=[]
+eleves["Corlings"]=[]
+eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
+eleves["Souleyman"]=[]
+eleves["Zack"]=[]
+def personne_elue(name):
+  return name == 'Zoureni'
 
-def main():
-  print('Informatique: le rêve')
+def search(name):
+  from collections import deque
+  search_queue = deque()
+  search_queue += eleves[name]
+  print( len(search_queue))
+  return False
 
 if __name__== "__main__":
-  main()
+  search("Boris")
